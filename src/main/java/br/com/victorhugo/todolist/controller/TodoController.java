85768191f3350847bfe2f2ecd44ed613f1cbf4c8
VictorN7 <victorhnogueira@hotmail.com>
2025-12-05 02:36:44 -0300

    Criado a Controller com as respectivas requisições

--- src/main/java/br/com/victorhugo/todolist/controller/TodoController.java
+++ src/main/java/br/com/victorhugo/todolist/controller/TodoController.java
@@ -1,12 +1,19 @@
 package br.com.victorhugo.todolist.controller;
-
 import java.util.List;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.victorhugo.todolist.entity.Todo;
 import br.com.victorhugo.todolist.service.TodoService;
 
+@RestController
+@RequestMapping("/todos")
 public class TodoController {
 	
 	private TodoService todoService; 
@@ -15,22 +22,23 @@ public class TodoController {
 		this.todoService = todoService;
 	}
 
-	@RestController("fdfdf")
-	public List<Todo> create(Todo todo){
+	@PostMapping
+	public List<Todo> create(@RequestBody Todo todo){
 		return todoService.create(todo);
 	}
 	
+	@GetMapping
 	public List<Todo> list(){
 		return todoService.list();
 	}
-		
-	public List<Todo> update(Todo todo){
+	
+	@PutMapping
+	public List<Todo> update(@RequestBody Todo todo){
 		return todoService.update(todo);
 	}
 	
-	public List<Todo> delete(Long id){
-		return todoService.delete(id);	
+	@DeleteMapping("{id}")
+	public List<Todo> delete(@PathVariable("id") Long id){
+		return todoService.delete(id);
 	}
-	
-
 }
--- src/main/java/br/com/victorhugo/todolist/service/TodoService.java
+++ src/main/java/br/com/victorhugo/todolist/service/TodoService.java
@@ -2,13 +2,6 @@ package br.com.victorhugo.todolist.service;
 import java.util.List;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import br.com.victorhugo.todolist.entity.Todo;
 import br.com.victorhugo.todolist.repository.TodoRepository;
 
@@ -21,27 +14,27 @@ public class TodoService {
 		this.todoRepository = todoRepository;
 	}
 
-	@PostMapping
-	public List<Todo> create(@RequestBody Todo todo){
+	
+	public List<Todo> create(Todo todo){
 		todoRepository.save(todo);
 		return list();
 	}
 	
-	@GetMapping
 	public List<Todo> list(){
 		Sort sort = Sort.by("prioridade").descending().and(Sort.by("nome").ascending());
 		return todoRepository.findAll(sort);
 	}
-	
-	@PutMapping
-	public List<Todo> update(@RequestBody Todo todo){
+		
+	public List<Todo> update(Todo todo){
 		todoRepository.save(todo);
 		return list();
 	}
 	
-	@DeleteMapping("{id}")
-	public List<Todo> delete(@PathVariable("id") Long id){
+	public List<Todo> delete(Long id){
+		
 		todoRepository.deleteById(id);;
 		return list();		
 	}
+	
+	
 }
